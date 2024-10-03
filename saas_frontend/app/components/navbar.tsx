8c352b70b69459ce9d0fe18de20e5f1da29e86c6
erwan blandeau <erwan.blandeau28@gmail.com>
2024-10-03 10:54:30 +0200

    ajout de la navbar refonte + css

--- saas_frontend/app/components/navbar.tsx
+++ saas_frontend/app/components/navbar.tsx
@@ -1,44 +1,83 @@
 "use client";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { Avatar, Dropdown, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 
 export default function NavbarApp() {
   return (
-    <Navbar className="py-2px px-5 bg-white shadow-md">
-      <NavbarContent className="flex justify-center items-center text-xl font-semibold text-black">
+    <Navbar fluid rounded className="bg-white shadow-lg">
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        {/* Brand / Logo */}
         <NavbarBrand className="flex items-center">
-          <img src="/logo.png" alt="logo" className="w-[200px] mr-[20px]" />
+          <img src="/logo.png" alt="logo" className="w-56 h-auto mr-6" />
         </NavbarBrand>
-        
-        <NavbarItem className="mr-5 hover:text-blue-600">
-          <Link href="#">
-            ?????
-          </Link>
-        </NavbarItem>
-        
-        <NavbarItem className="mr-5 text-blue-600">
-          <Link href="#" aria-current="page">
-            ?????
-          </Link>
-        </NavbarItem>
 
-        <NavbarItem className="mr-5 hover:text-blue-600">
-          <Link href="#">
-            ?????
-          </Link>
-        </NavbarItem>
+        {/* Middle Navigation Links */}
+        <div className="hidden md:flex space-x-6">
+          <NavbarLink href="#" active className="text-lg font-semibold text-gray-900 hover:text-blue-600">
+            Home
+          </NavbarLink>
+          <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+            About
+          </NavbarLink>
+          <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+            Services
+          </NavbarLink>
+          <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+            Pricing
+          </NavbarLink>
+          <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600 ">
+            Contact
+          </NavbarLink>
+        </div>
 
-        <NavbarItem className="mr-5">
-          <Button as={Link} href="#" className="bg-blue-600 text-white py-2 px-4 rounded transition duration-300 hover:bg-blue-700">
-            Login
-          </Button>
-        </NavbarItem>
+        {/* Right Section (Dropdown / Toggle for Mobile) */}
+        <div className="flex items-center ml-[20px] space-x-4">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded
+                className="h-10 w-10"
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm font-medium text-gray-900">Bonnie Green</span>
+              <span className="block truncate text-sm text-gray-500">name@flowbite.com</span>
+            </Dropdown.Header>
+            <Dropdown.Item className="hover:bg-gray-100">Dashboard</Dropdown.Item>
+            <Dropdown.Item className="hover:bg-gray-100">Settings</Dropdown.Item>
+            <Dropdown.Item className="hover:bg-gray-100">Earnings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="hover:bg-gray-100">Sign out</Dropdown.Item>
+          </Dropdown>
+          <NavbarToggle />
+        </div>
+      </div>
 
-        <NavbarItem className="mr-5">
-          <Button as={Link} href="#" className="bg-blue-600 text-white py-2 px-4 rounded transition duration-300 hover:bg-blue-700">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+      {/* Mobile Menu */}
+      <NavbarCollapse className="md:hidden flex flex-col space-y-4 bg-white px-6 py-4">
+        <NavbarLink href="#" active className="text-lg font-semibold text-gray-900 hover:text-blue-600">
+          Home
+        </NavbarLink>
+        <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+          About
+        </NavbarLink>
+        <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+          Services
+        </NavbarLink>
+        <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+          Pricing
+        </NavbarLink>
+        <NavbarLink href="#" className="text-lg text-gray-700 hover:text-blue-600">
+          Contact
+        </NavbarLink>
+      </NavbarCollapse>
     </Navbar>
   );
 }
+
+
+
--- saas_frontend/app/inscription/page.tsx
+++ saas_frontend/app/inscription/page.tsx
@@ -1,6 +1,5 @@
 import InscriptionForm from "../components/inscription_form";
 
-InscriptionForm
 export default function Inscription() {
     return (
         <InscriptionForm/>
