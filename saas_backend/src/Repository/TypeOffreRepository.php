3ae23dfcb28998f4d0fe81aa6bddeabd1e0787cb
marmionsteven8 <marmionsteven8@gmail.com>
2024-10-21 20:45:22 +0200

    Finition du repository CRUD

--- saas_backend/src/Repository/TypeOffreRepository.php
+++ saas_backend/src/Repository/TypeOffreRepository.php
@@ -16,28 +16,79 @@ class TypeOffreRepository extends ServiceEntityRepository
         parent::__construct($registry, TypeOffre::class);
     }
 
-//    /**
-//     * @return TypeOffre[] Returns an array of TypeOffre objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * Trouve une offre par son type.
+     *
+     * @param string $type Le type de l'offre à rechercher.
+     * @return TypeOffre[] Une liste d'offres correspondant aux critères.
+     */
+    public function trouveOffreByType(string $type): array
+    {
+        try {
+            return $this->createQueryBuilder('t')
+                ->andWhere('t.type = :type')
+                ->setParameter('type', $type)
+                ->getQuery()
+                ->getResult();
+        } catch (\Exception $e) {
+            throw new \Exception("Erreur lors de la récupération de l'offre", $e->getCode());
+        }
+    }
 
-//    public function findOneBySomeField($value): ?TypeOffre
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * Inscrit une nouvelle offre dans la base de données.
+     *
+     * @param TypeOffre $offre L'objet de l'offre à persister.
+     * @return bool Indique si l'inscription a réussi.
+     *
+     * @throws \RuntimeException Si une erreur survient lors de l'enregistrement.
+     */
+    public function inscritOffre(TypeOffre $offre): bool
+    {
+        try {
+            $this->getEntityManager()->persist($offre);
+            $this->getEntityManager()->flush();
+            return true;
+        } catch (\Exception $e) {
+            throw new \RuntimeException("Erreur lors de l'enregistrement de l'offre : " . $e->getCode());
+        }
+    }
+
+    /**
+     * Met à jour une offre existante dans la base de données.
+     *
+     * @param TypeOffre $offre L'objet de l'offre à mettre à jour.
+     * @return bool|null Indique si la mise à jour a réussi.
+     *
+     * @throws \Exception Si une erreur survient lors de la mise à jour.
+     */
+    public function updateOffre(TypeOffre $offre): ?bool
+    {
+        try {
+            $this->getEntityManager()->persist($offre);
+            $this->getEntityManager()->flush();
+            return true;
+        } catch (\Exception $e) {
+            throw new \Exception("Erreur lors de l'update de l'offre", $e->getCode());
+        }
+    }
+
+    /**
+     * Supprime une offre de la base de données.
+     *
+     * @param TypeOffre $offre L'offre à supprimer.
+     * @return bool|null Indique si la suppression a réussi.
+     *
+     * @throws \RuntimeException Si une erreur survient lors de la suppression.
+     */
+    public function removeOffre(TypeOffre $offre): ?bool
+    {
+        try {
+            $this->getEntityManager()->remove($offre);
+            $this->getEntityManager()->flush();
+            return true;
+        } catch (\Exception $e) {
+            throw new \RuntimeException("Erreur lors de la suppression de l'offre", $e->getCode());
+        }
+    }
 }
