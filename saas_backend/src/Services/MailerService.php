fe669840034144c409de36b888f930f662a18d9c
stevenMarmion <marmionsteven07@gmail.com>
2024-11-11 16:13:54 +0100

    Ajout du serveur SMTP fonctionnel

---
+++ saas_backend/src/Services/MailerService.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+
+class MailerService
+{
+    private MailerInterface $mailer;
+    private string $fromEmail;
+    private string $fromName;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+        $this->fromEmail = "marmionsteven8@gmail.com"; // à changer lors de la mise en route de l'applciation
+        $this->fromName = "UMODJA";
+    }
+
+    /**
+     * Envoi un email à la destination voulue, avec l'objet du mail voulu et le contenu
+     * @param string $to La destination du mail
+     * @param string $subject L'objet du mail
+     * @param string $htmlContent Le contenu du mail
+     * @return void
+     */
+    public function sendEmail(string $to, string $subject, string $content): JsonResponse
+    {
+        try {
+            $email = (new Email())
+                ->from(new Address($this->fromEmail, $this->fromName))
+                ->to($to)
+                ->subject($subject)
+                ->html($content);
+
+            $this->mailer->send($email);
+
+            return new JsonResponse([
+                'status' => 'success', 
+                'message' => 'E-mail envoyé avec succès.'
+            ]);
+        } catch (\Exception $e) {
+            throw new \RuntimeException(''. $e->getMessage());
+        }
+    }
+}
