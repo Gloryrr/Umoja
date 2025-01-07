c06b83f7920fa3930b1ebbec4cdc08bf2b0565fe
stevenMarmion <marmonsteven07@gmail.com>
2025-01-07 22:15:24 +0100

    Ajout des templates de création et de modification de projet pour les notifs par mail + séparation création d'un projet avec l'envoie de mail -> gain de temps de 3sec

--- saas_backend/src/Controller/EmailController.php
+++ saas_backend/src/Controller/EmailController.php
@@ -2,30 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\OffreRepository;
 use App\Services\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EmailController extends AbstractController
 {
-    #[Route('/api/v1/envoi-email', name: 'envoi_email', methods: ['POST'])]
-    public function sendEmail(
+    #[Route('/api/v1/envoi-email-new-projet', name: 'envoi_email_nouveau_projet', methods: ['POST'])]
+    public function sendEmailNewProjet(
+        OffreRepository $offreRepository,
+        Request $request,
         MailerService $mailerService,
     ): JsonResponse {
-        // changer la destination dynamiquement en fonction de l'utilisateur
-        $to = 'marmionsteven07@gmail.com';
+        $data = json_decode($request->getContent(), true);
+        return $mailerService->sendEmailNewProjet(
+            $offreRepository,
+            $data,
+        );
+    }
 
-        // changer le sujet dynamiquement en fonction de la notification de l'application
-        $subject = "Test d'envoi d'e-mail avec Gmail";
-
-        // changer le contenu en fonction de l'utilité du mail, voir si on peut utiliser des templates ???
-        $content = "<h1>Ceci est un test</h1><p>L'e-mail a été envoyé avec succès via Gmail SMTP. TEST TEST TEST</p>";
-
-        return $mailerService->sendEmail(
-            $to,
-            $subject,
-            $content
+    #[Route('/api/v1/envoi-email-update-projet', name: 'envoi_email_update_projet', methods: ['POST'])]
+    public function sendEmailUpdateProjet(
+        OffreRepository $offreRepository,
+        Request $request,
+        MailerService $mailerService,
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+        return $mailerService->sendEmailUpdateProjet(
+            $offreRepository,
+            $data,
         );
     }
 }
--- saas_backend/src/Services/MailerService.php
+++ saas_backend/src/Services/MailerService.php
@@ -9,6 +9,7 @@ use Symfony\Component\Mime\Address;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
 use App\Repository\UtilisateurRepository;
+use App\Repository\OffreRepository;
 
 class MailerService
 {
@@ -24,22 +25,134 @@ class MailerService
     }
 
     /**
-     * Envoi un email à la destination voulue, avec l'objet du mail voulu et le contenu
+     * Envoi un email pour notifier de la création d'un projet, avec l'objet du mail voulu et le contenu
      * @param string $to La destination du mail
      * @param string $subject L'objet du mail
      * @param string $htmlContent Le contenu du mail
      * @return void
      */
-    public function sendEmail(string $to, string $subject, string $content): JsonResponse
-    {
+    public function sendEmailNewProjet(
+        OffreRepository $offreRepository,
+        mixed $data,
+    ): JsonResponse {
         try {
-            $email = (new Email())
-                ->from(new Address($this->umodjaEmail, $this->umodjaName))
-                ->to($to)
-                ->subject($subject)
-                ->html($content);
+            // Préparation de la template HTML
+            $templatePath = __DIR__ . '/../../templates/emails/notification_creation_projet.html.twig';
+            $htmlTemplate = file_get_contents($templatePath);
 
-            $this->mailer->send($email);
+            $subject = "Création d'un nouveau projet sur Umodja";
+
+            // Préparation de l'envoi de l'email à tous les utilisateurs concernés
+            $offre = $offreRepository->find($data['offreId']);
+            if (!$offre) {
+                return new JsonResponse(['error' => 'Offre non trouvée'], Response::HTTP_NOT_FOUND);
+            }
+
+            $reseaux_list = $offre->getReseaux();
+
+            foreach ($reseaux_list as $reseau) {
+                $utilisateurs = $reseau->getUtilisateurs();
+                foreach ($utilisateurs as $utilisateur) {
+                    if ($utilisateur->getEmailUtilisateur() != null) {
+                        $htmlMessage = str_replace(
+                            [
+                                '{{projectName}}', 
+                                '{{projectDescription}}',
+                                '{{userName}}', 
+                                '{{currentYear}}', 
+                                '{{emailUmodja}}',
+                                '{{networkName}}'
+                            ],
+                            [
+                                $data['projectName'], 
+                                $data['projectDescription'],
+                                $data['username'], 
+                                date('Y'), 
+                                $this->umodjaEmail,
+                                $reseau->getNomReseau()
+                            ],
+                            $htmlTemplate
+                        );
+                        $email = (new Email())
+                            ->from(new Address($this->umodjaEmail, $this->umodjaName))
+                            ->to($utilisateur->getEmailUtilisateur())
+                            ->subject($subject)
+                            ->html($htmlMessage);
+
+                        $this->mailer->send($email);
+                    }
+                }
+            }
+
+            return new JsonResponse([
+                'mail' => 'succès',
+                'message' => 'E-mail envoyé avec succès.'
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            throw new \RuntimeException('' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Envoi un email pour notifier des modifications d'un projet, avec l'objet du mail voulu et le contenu
+     * @param string $to La destination du mail
+     * @param string $subject L'objet du mail
+     * @param string $htmlContent Le contenu du mail
+     * @return void
+     */
+    public function sendEmailUpdateProjet(
+        OffreRepository $offreRepository,
+        mixed $data,
+    ): JsonResponse {
+        try {
+            // Préparation de la template HTML
+            $templatePath = __DIR__ . '/../../templates/emails/notification_modification_projet.html.twig';
+            $htmlTemplate = file_get_contents($templatePath);
+
+            $subject = "Modification d'un projet sur Umodja";
+
+            // Préparation de l'envoi de l'email à tous les utilisateurs concernés
+            $offre = $offreRepository->find($data['offreId']);
+
+            if (!$offre) {
+                return new JsonResponse(['error' => 'Offre non trouvée'], Response::HTTP_NOT_FOUND);
+            }
+
+            $reseaux_list = $offre->getReseaux();
+
+            foreach ($reseaux_list as $reseau) {
+                $utilisateurs = $reseau->getUtilisateurs();
+                foreach ($utilisateurs as $utilisateur) {
+                    if ($utilisateur->getEmailUtilisateur() != null) {
+                        $htmlMessage = str_replace(
+                            [
+                                '{{projectName}}', 
+                                '{{projectDescription}}',
+                                '{{userName}}', 
+                                '{{currentYear}}', 
+                                '{{emailUmodja}}',
+                                '{{networkName}}'
+                            ],
+                            [
+                                $data['projectName'], 
+                                $data['projectDescription'], 
+                                $data['username'], 
+                                date('Y'), 
+                                $this->umodjaEmail,
+                                $reseau->getNomReseau()
+                            ],
+                            $htmlTemplate
+                        );
+                        $email = (new Email())
+                            ->from(new Address($this->umodjaEmail, $this->umodjaName))
+                            ->to($utilisateur->getEmailUtilisateur())
+                            ->subject($subject)
+                            ->html($htmlMessage);
+
+                        $this->mailer->send($email);
+                    }
+                }
+            }
 
             return new JsonResponse([
                 'mail' => 'succès',
